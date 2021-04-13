b8e6216db233c7808c5b84033939f420a32cfaea
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-13 15:13:56 +0200

    aa

--- CasinoRoyal/Controllers/HomeController.cs
+++ CasinoRoyal/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CasinoRoyal.Controllers
 {
@@ -23,6 +24,7 @@ namespace CasinoRoyal.Controllers
             return View();
         }
 
+        [Authorize("IsReceptionist")]
         public IActionResult Receptionist()
         {
             return View();
@@ -33,6 +35,7 @@ namespace CasinoRoyal.Controllers
             return View();
         }
 
+        [Authorize("IsWaiter")]
         public IActionResult Waiter()
         {
             return View();
