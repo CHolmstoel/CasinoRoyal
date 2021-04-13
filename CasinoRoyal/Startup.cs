7efc226ef78304e4850f0323d4f5ee33c6341dee
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-13 15:58:25 +0200

    added waiter seed user

--- CasinoRoyal/Startup.cs
+++ CasinoRoyal/Startup.cs
@@ -127,6 +127,28 @@ namespace CasinoRoyal
                 }
             }
 
+            const string waiterUserEmail = "waiter@waiter.com";
+            const string waiterPassword = "Sommer25!";
+            const string waiterUserCell = "20212223";
+            const string waiterUserName = "Yrsa Poulsen";
+
+            if (userManager.FindByNameAsync(waiterUserEmail).Result == null)
+            {
+                var user3 = new ApplicationUser();
+                user3.UserName = waiterUserEmail;
+                user3.Email = waiterUserEmail;
+                user3.EmailConfirmed = emailConfirmed;
+                user3.PhoneNumber = waiterUserCell;
+                user3.Name = waiterUserName;
+
+                IdentityResult result = userManager.CreateAsync(user3, waiterPassword).Result;
+
+                if (result.Succeeded) //Add claim to user
+                {
+                    userManager.AddClaimAsync(user3, new Claim("Waiter", "IsWaiter"));
+                }
+            }
+
         }
     }
 }
