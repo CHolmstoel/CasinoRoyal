6cb356dd09ad5026634c2f84806af51d80d4e2df
Camilla <camilla.holmstoel@hotmail.com>
2021-04-13 11:27:50 +0200

    Repos Added

---
+++ CasinoRoyal/Data/Repositories/RepositoryInterfaces/IHotelRoomRepository.cs
@@ -0,0 +1,15 @@
+﻿using CasinoRoyal.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasinoRoyal.Data.Repositories.RepositoryInterfaces
+{
+    interface IHotelRoomRepository: IRepository<HotelRoom>
+    {
+        public List<HotelRoom> GetAllHotelRooms();
+        public HotelRoom GetSingleHotelRoom(int id);
+        public void AddHotelRoom(HotelRoom hotelRoom);
+    }
+}
